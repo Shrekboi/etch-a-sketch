e7099ac7cd62eaa51ac6f8fffd2421ed7a3d8b79
Shrekboi <programming.shek@gmail.com>
2022-01-04 17:36:09 +0200

    resizing feature works

--- js.js
+++ js.js
@@ -1,12 +1,13 @@
 const container = document.querySelector(".div-container")
 const clearButton = document.querySelector(".actualButton")
 
-function createDiv() {
+function createDivColumn(times) {
 
-    for (let i = 0; i < 16; i++) {
+    for (let i = 0; i < times; i++) {
+        let boxSize = 600 - (times * 2)
         const div = document.createElement("div")
-        div.style.width = 568 / 16 + "px"
-        div.style.height = 568 / 16 + "px"
+        div.style.width = boxSize / times + "px"
+        div.style.height = boxSize / times + "px"
         div.style.border = "black 1px solid"
         div.classList.add("square")
 
@@ -14,29 +15,43 @@ function createDiv() {
     }
 }
 
-for (let i = 0; i < 16; i++) {
-    createDiv()
+function createGrid(times) {
+
+    for (let i = 0; i < times; i++) {
+        createDivColumn(times)
+    }
 }
 
-
-
-
-const squares = document.querySelectorAll(".square")
-
-squares.forEach(squares => {
-    squares.addEventListener("mouseover", () => {
-        let randomColor = Math.floor(Math.random()*16777215).toString(16);
-        squares.style.background = "#" + randomColor
-    })
-})
+createGrid(16)
 
 function resetSquares() {
-    squares.forEach(squares => {
-        squares.style.background = "white"
+    while (container.hasChildNodes()) {
+        container.removeChild(container.lastChild)
     }
-    )
 }
 
 clearButton.addEventListener("click", () => {
     resetSquares();
+    let newGridSize = prompt("New grid size. Less than 60.")
+    if (newGridSize <= 60) {
+        createGrid(newGridSize);
+        updateTheSquares();
+    }
+
 })
+
+function updateTheSquares() {
+
+    const squares = document.querySelectorAll(".square")
+
+    squares.forEach(squares => {
+        squares.addEventListener("mouseover", () => {
+            let randomColor = Math.floor(Math.random() * 16777215).toString(16);
+            squares.style.background = "#" + randomColor
+        })
+    })
+
+
+}
+
+updateTheSquares();

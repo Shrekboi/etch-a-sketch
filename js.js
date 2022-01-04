9a9a5a84ddc6fd53f21230fd02be5830a57e0c52
Shrekboi <programming.shek@gmail.com>
2022-01-04 14:55:21 +0200

    added the drawing feature

--- js.js
+++ js.js
@@ -2,36 +2,24 @@ const container = document.querySelector(".div-container")
 
 function createDiv() {
 
-
     for (let i = 0; i < 16; i++) {
         const div = document.createElement("div")
         div.style.width = 568 / 16 + "px"
         div.style.height = 568 / 16 + "px"
         div.style.border = "black 1px solid"
+        div.classList.add("square")
 
         container.appendChild(div);
-
     }
-
-
 }
-
 
 for (let i = 0; i < 16; i++) {
     createDiv()
-
 }
 
+const squares = document.querySelectorAll(".square")
 
-
-
-
-
-// // const container = document.querySelector('.div-container');
-
-// // const content = document.createElement('div');
-// // content.classList.add('content');
-// // content.textContent = 'This is the glorious text-content!';
-
-// // container.appendChild(content);
-
+squares.forEach(squares => {
+    squares.addEventListener("mouseover", () => {
+    squares.style.background = "red" })
+})
